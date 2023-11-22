3104334611255b8ccfc1d4b48aa1af6997e47e97
adrien alard <adrien.alard@gmail.com>
2023-11-22 17:03:07 +0100

    add anim things

--- Assets/Scripts/AgentAnimator.cs
+++ Assets/Scripts/AgentAnimator.cs
@@ -2,8 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AnimType
+{
+    Walk,
+    Jump,
+    BeginJump,
+    Fall,
+    Swim
+}
+
 public class AgentAnimator : MonoBehaviour
 {
     [SerializeField]
     Animator animator;
+
+    string walk;
+    string jump;
+    string beginJump;
+    string fall;
+    string swim;
+
+    public void ChangeAnimState()
+    {
+
+    }
 }
