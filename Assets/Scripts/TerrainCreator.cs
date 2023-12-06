659a54cf5ab68ba74f89fa581dd14b49c636acfb
Rémi Leprévost <79415625+mechawhillem@users.noreply.github.com>
2023-12-06 16:50:01 +0100

    Change terrainCreator to add arcDegrees

--- Assets/Scripts/TerrainCreator.cs
+++ Assets/Scripts/TerrainCreator.cs
@@ -1,46 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class TerrainCreator : MonoBehaviour
 {
-    [SerializeField]
-    int segments = 100;
-    [SerializeField]
-    float outerRadius = 1f;
-    [SerializeField]
-    float innerRadius = 0.5f;
+    [SerializeField] float outerRadius = 5f;
+    [SerializeField] float innerRadius = 2f;
+    [SerializeField, Range(3f, 100f)] int numberOfSegments = 100;
+    [SerializeField, Range(0f, 360f)] float arcDegrees = 360f;
 
+    private MeshFilter meshFilter;
 
-    [ContextMenu("drawMesh")]
-    void MakeMesh()
+    void Start() 
     {
-        MeshFilter mf = GetComponent<MeshFilter>();
-        Mesh mesh = new Mesh();
-        mf.mesh = mesh;
+        GenerateCircleWithHole();
+    }
 
-        Vector3[] vertices = new Vector3[segments * 2];
-        int[] triangles = new int[segments * 6];
-
-        float angleStep = 360.0f / segments;
-        for (int i = 0; i < segments; i++)
+    void Update()
+    {
+        if (Application.isEditor)
         {
-            float angle = i * angleStep * Mathf.Deg2Rad;
+            GenerateCircleWithHole();
+        }
+    }
 
-            vertices[i * 2] = new Vector3(Mathf.Cos(angle) * outerRadius, 0, Mathf.Sin(angle) * outerRadius);
-            vertices[i * 2 + 1] = new Vector3(Mathf.Cos(angle) * innerRadius, 0, Mathf.Sin(angle) * innerRadius);
+    void GenerateCircleWithHole()
+    {
+        Mesh mesh = new Mesh();
+        meshFilter.mesh = mesh;
 
-            triangles[i * 6] = i * 2;
-            triangles[i * 6 + 1] = (i * 2 + 3) % (segments * 2);
-            triangles[i * 6 + 2] = i * 2 + 1;
-            triangles[i * 6 + 3] = i * 2;
-            triangles[i * 6 + 4] = (i * 2 + 2) % (segments * 2);
-            triangles[i * 6 + 5] = (i * 2 + 3) % (segments * 2);
+        // Calculate the number of segments based on the arc
+        int actualSegments = Mathf.CeilToInt((numberOfSegments * arcDegrees) / 360f);
+        float deltaTheta = Mathf.Deg2Rad * arcDegrees / actualSegments;
+        float theta = 0f;
+
+        // Vertices
+        Vector3[] vertices = new Vector3[(actualSegments + 1) * 2]; // Increase vertices count by 1
+
+        for (int i = 0; i <= actualSegments; i++) // Change loop condition
+        {
+            float xOuter = Mathf.Cos(theta) * outerRadius;
+            float yOuter = Mathf.Sin(theta) * outerRadius;
+
+            float xInner = Mathf.Cos(theta) * innerRadius;
+            float yInner = Mathf.Sin(theta) * innerRadius;
+
+            vertices[i] = new Vector3(xOuter, yOuter, 0f);
+            vertices[i + actualSegments + 1] = new Vector3(xInner, yInner, 0f); // Increase index by 1
+
+            theta += deltaTheta;
         }
 
+        // Triangles
+        int[] triangles = new int[actualSegments * 6];
+        for (int i = 0, ti = 0; i < actualSegments; i++, ti += 6)
+        {
+            int next = (i + 1) % (actualSegments + 1); // Change loop condition
+
+            triangles[ti] = i;
+            triangles[ti + 1] = i + actualSegments + 1;
+            triangles[ti + 2] = next + actualSegments + 1;
+
+            triangles[ti + 3] = i;
+            triangles[ti + 4] = next + actualSegments + 1;
+            triangles[ti + 5] = next;
+        }
+
+        // Assigning data to the mesh
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+
         mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
     }
 }
