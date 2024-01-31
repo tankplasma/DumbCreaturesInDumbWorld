294a7eb3a37fc2858600e1650f20ddab3313fce5
Rémi Leprévost <79415625+mechawhillem@users.noreply.github.com>
2024-01-31 16:14:23 +0100

    add level 4

---
+++ Assets/Scenes/AllLevels/Tets.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tets : MonoBehaviour
+{
+    [SerializeField] public GameObject test { get; set; }
+}
