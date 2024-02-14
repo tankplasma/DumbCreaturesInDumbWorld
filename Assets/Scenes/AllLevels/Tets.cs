8789cc08cade85694434881019110731d923bab7
Rémi Leprévost <79415625+mechawhillem@users.noreply.github.com>
2024-02-14 11:18:19 +0100

    idk

--- Assets/Scenes/AllLevels/Tets.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tets : MonoBehaviour
-{
-    [SerializeField] public GameObject test { get; set; }
-}
